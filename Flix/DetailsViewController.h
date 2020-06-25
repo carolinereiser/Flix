3b1b9bbea583dfb21c7019377b02eace25b0a896
Caroline Reiser <carolinereiser@fb.com>
2020-06-24 18:43:35 -0700

    detail view when clicking on a movie

---
+++ Flix/DetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by Caroline Reiser on 6/24/20.
+//  Copyright © 2020 Caroline Reiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
